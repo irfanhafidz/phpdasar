643decaddb676be034826c805ad3390d761b804c
Irfanuddin Hafidz <zauhar.h@gmail.com>
2021-06-27 08:26:43 +0700

    #13 Membuat Fitur Upload

---
+++ pertemuan13/functions.php
@@ -0,0 +1,128 @@
+<?php 
+//XAMPP
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+function query($query) {
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $container = [];
+    while( $baris = mysqli_fetch_assoc($result) ){
+    $container[] = $baris;
+    }
+    return $container;
+}
+
+function input($data) {
+    global $conn;
+    //membuat container array
+    $fname = htmlspecialchars($data["fname"]);
+    $lname = htmlspecialchars($data["lname"]);
+    $nik = htmlspecialchars($data["nik"]);
+    $kelas = htmlspecialchars($data["kelas"]);
+    $email = htmlspecialchars($data["email"]);
+    $foto = upload();
+    if(!$foto) {
+        return false;
+    }
+
+    //query insert data
+    $query = "INSERT INTO penduduk VALUES
+    ('', '$fname', '$lname', '$nik', '$kelas', '$email', '$foto')";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+
+function upload(){
+    $nameFile = $_FILES['foto']['name'];
+    $sizeFile = $_FILES['foto']['size'];
+    $error = $_FILES['foto']['error'];
+    $tmpName = $_FILES['foto']['tmp_name'];
+
+    if( $error === 4 ) {
+        echo "<script>
+        alert('Silahkan masukkan foto!');
+        </script>";
+        return false;
+    }
+
+    $extImageValid = ['jpg', 'jpeg', 'png'];
+    $extImage = explode('.', $nameFile);
+    $extImage = strtolower(end($extImage));
+    if( !in_array($extImage, $extImageValid) ){
+            echo "<script>
+                alert('yang anda upload bukan gambar')
+                 </script>" ;
+    return false;
+    }
+
+    if($sizeFile > 10000000) {
+        echo "<script>
+            alert('ukuran gambar terlalu besar')
+             </script>" ;
+    return false;
+    }
+
+    $nameFileNew = uniqid();
+    $nameFileNew .= '.';
+    $nameFileNew .= $extImage;
+
+
+    move_uploaded_file($tmpName, 'img/' . $nameFileNew);
+    return $nameFileNew;
+
+
+}
+
+function delete($id) {
+    global $conn;
+    mysqli_query($conn, "DELETE FROM penduduk WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
+
+function modify($data) {
+    global $conn;
+    //membuat container array
+    $id = ($data["id"]);
+    $fname = htmlspecialchars($data["fname"]);
+    $lname = htmlspecialchars($data["lname"]);
+    $nik = htmlspecialchars($data["nik"]);
+    $kelas = htmlspecialchars($data["kelas"]);
+    $email = htmlspecialchars($data["email"]);
+    $fotoOld = htmlspecialchars($data["fotoOld"]);
+
+
+    if($_FILES['foto']['error'] === 4 ){
+        $foto = $fotoOld;   
+        } else {
+            $foto = upload();
+        }
+
+    //query update data
+    $query = "UPDATE penduduk SET
+        fname = '$fname',
+        lname = '$lname',
+        nik = '$nik',
+        kelas = '$kelas',
+        email = '$email',
+        foto = '$foto'
+        WHERE id = $id
+        ";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function search($keyword) {
+    $query = "SELECT * FROM penduduk
+    WHERE
+    fname LIKE '%$keyword%' OR
+    lname LIKE '%$keyword%' OR
+    nik LIKE '%$keyword%' OR
+    kelas LIKE '%$keyword%'
+    ";
+    return query($query);
+}
+
+?>
---
+++ pertemuan13/index.php
@@ -0,0 +1,56 @@
+<?php
+require "functions.php";
+$penduduk = query("SELECT * FROM penduduk");
+
+if(isset($_POST["search"]) ) {
+    $penduduk = search($_POST["keyword"]);
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styla.css">
+    <title>Document</title>
+</head>
+<body>
+<h1>Daftar Penduduk</h1>
+<?php $i = 1; ?>
+<button onclick="location.href='inputdata.php'">Tambah Data</button>
+<form class="searchbar" action="" method="post">
+    <input type="text" name="keyword" size="30" placeholder="Masukkan keyword pencarian.." autofocus autocomplete="off">
+    <button type="submit" name="search">Search</button>
+</form>
+<table class="table" border="1px" cellpadding="10" cellspacing="0">
+<tr>
+    <th>No.</th>
+    <th>Foto</th>
+    <th>Nama</th>
+    <th>NIK</th>
+    <th>Kelas</th>
+    <th>Email</th>
+    <th>Action</th>
+    <?php foreach($penduduk as $baris):?>
+</tr>
+    <td><?= $i; ?></td>
+    <td><img class="foto" src="img/<?=$baris["foto"];?>"></td>
+    <td><?= $baris["fname"]." ".$baris["lname"];?></td>
+    <td><?= $baris["nik"];?></td>
+    <td><?= $baris["kelas"];?></td>
+    <td><?= $baris["email"];?></td>
+    <td>
+        <a href="modify.php?id=<?=$baris["id"]?>">Ubah</a> | 
+        <a href="delete.php?id=<?=$baris["id"]?>" onclick="return confirm('Yakin ingin menghapus?');">Hapus</a>
+    </td>
+<tr>
+<?php $i++ ?>
+<?php endforeach ?>
+</tr>
+</table>
+</body>
+</html>
